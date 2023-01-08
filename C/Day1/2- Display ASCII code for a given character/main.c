46a9ab2653c1b6d32a965f71392b40a4c0d1eaa2
Ziad Eleraky <ziadeleraky1@gmail.com>
2023-01-08 14:02:44 +0200

    C - Day1

---
+++ C/Day1/2- Display ASCII code for a given character/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+    printf("Enter a Character: ");
+    scanf("%c", &ch);
+    printf("ASCII for %c is: %d", ch, ch);
+    return 0;
+}
---
+++ C/Day1/3- Display Hexadecimal representation for a given integer/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    printf("Enter an Integer: ");
+    scanf("%d", &x);
+    printf("Hexadecimal: %x", x);
+    return 0;
+}
