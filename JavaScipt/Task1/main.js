d6dd4fe192f157181b2b905802e43352fe38558f
Ziad Eleraky <ziadeleraky1@gmail.com>
2023-01-13 13:11:42 +0200

    Day1 - Task1

---
+++ JavaScipt/Task1/main.js
@@ -0,0 +1,5 @@
+var headerMsg = prompt("Enter a Message");
+for (var i = 1; i <= 6; i++) {
+  // document.write(`<h${i}>${headerMsg}</h${i}>`);
+  document.write("<h"+i+">"+headerMsg+"</h"+i+">");
+}
