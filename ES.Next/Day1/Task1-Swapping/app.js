426feebed31998ad8808225135f3bd28da38a13e
Ziad Eleraky <ziadeleraky1@gmail.com>
2023-01-24 12:24:50 +0200

    Day1 - Task1

---
+++ ES.Next/Day1/Task1-Swapping/app.js
@@ -0,0 +1,8 @@
+let x = 5;
+let y = 10;
+console.log("x - before", x);
+console.log("y - before", y);
+// Swapp Using Destructuring
+[x, y] = [y, x];
+console.log("x - after", x);
+console.log("y - after", y);
