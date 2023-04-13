c73f5dbe09a20ca268e595765288c1df4f54fd34
Ziad Eleraky <ziadeleraky1@gmail.com>
2023-04-13 13:38:15 +0200

    Day2 - Task3 - React Testing

---
+++ Unit Testing/Day2/Task3 - React/src/TestEvents.test.js
@@ -0,0 +1,20 @@
+import TestEvents from "./TestEvents";
+import { fireEvent, render } from "@testing-library/react";
+
+describe("test TestEvents", () => {
+  it("test button-up", () => {
+    const { getByTestId } = render(<TestEvents />);
+    fireEvent.click(getByTestId("button-up"));
+    expect(getByTestId("counter")).toHaveTextContent('1')
+  });
+});
+
+describe("test TestEvents", () => {
+  it("test button-down", () => {
+    const { getByTestId } = render(<TestEvents />);
+    fireEvent.click(getByTestId("button-down"));
+    expect(getByTestId("counter")).toHaveTextContent('-1')
+  });
+});
+
+
